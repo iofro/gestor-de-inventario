ca91d3e8b39d006eb0abc2900b625708f9422aaa
iofro <arieliosefrosales@gmail.com>
2025-08-18 16:28:10 -0600

    cambios

--- tests/test_db_smoke.py
+++
@@ -1,5 +0,0 @@
-import db
-
-
-def test_db_module_imports():
-    assert hasattr(db, "DB")
--- tests/test_dialogs_smoke.py
+++
@@ -1,5 +0,0 @@
-import dialogs
-
-
-def test_dialogs_module_imports():
-    assert hasattr(dialogs, "ClienteDialog")

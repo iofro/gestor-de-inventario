a4cda672245de3d6028f74af80cd9baff2006783
iofro <arieliosefrosales@gmail.com>
2025-09-10 15:35:45 -0600

    Omitir DUI en emisor

--- dte.py
+++ dte.py
@@ -2646,10 +2646,8 @@ def validate_dte_json(
     emisor = payload.get("emisor", {})
     emisor["nit"] = _clean_nit(emisor.get("nit") or negocio.get("nit"))
     emisor["nrc"] = _clean_nrc(emisor.get("nrc") or negocio.get("nrc"))
-    if tipo_dte != "03":
-        emisor["dui"] = _clean_dui(emisor.get("dui") or negocio.get("dui"))
-    else:
-        emisor.pop("dui", None)
+    # ``dui`` is not permitted for the emisor by the DTE schema
+    emisor.pop("dui", None)
     emisor.setdefault("nombre", negocio.get("nombre"))
     emisor.setdefault("nombreComercial", negocio.get("nombreComercial"))
     emisor.setdefault(
